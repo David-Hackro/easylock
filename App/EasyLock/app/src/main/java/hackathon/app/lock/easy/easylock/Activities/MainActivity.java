4281f5f508962a919940bf4173c0f971353eaf4f
josedlujan <josedlujan@gmail.com>
2016-06-20 21:15:46 -0500

    Splash

---
+++ App/EasyLock/app/src/main/java/hackathon/app/lock/easy/easylock/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package hackathon.app.lock.easy.easylock.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hackathon.app.lock.easy.easylock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
